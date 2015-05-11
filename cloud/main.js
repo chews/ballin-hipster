e64837785dbb8f97ff36d463863644c012b2d6a8
genevievelesperance <genevieve.lesperance@mail.mcgill.ca>
2015-05-11 09:50:14 -0700

    Cleaning up code and attempting TriadScore

--- cloud/main.js
+++ cloud/main.js
@@ -6,28 +6,6 @@ ig.initialize('e098fc55f76442deaf8057c0fcaeeb76');
 ig.setAccessToken('261345369.dc8bd7d.d743823297444ed1acc38b9b0e1f5ef2');
 
 var FIREBASE_URL = 'https://apis.firebaseio.com';
-Parse.Cloud.beforeSave("Chat", function(request, response){
-    if(request.object.get("name") === "cia"){
-        request.object.set("message", "redacted");
-    }
-    response.success();
-}); 
-
-// Parse.Cloud.afterSave("Chat", function(request) {
-//     var jsn = request.object.get("message")
-//     console.log('=================================' + jsn)
-//     Parse.Cloud.httpRequest({
-//         method: 'POST',
-//         url: FIREBASE_URL + '/chat.json',
-//         body: '{"test":"complete"}'
-//     }).then(function(httpResponse) {
-//         console.log('Success!');
-//     },
-//     function(httpResponse) {
-//         console.error('Request failed with response code ' + httpResponse.status);
-//     });
-// });
-
 
 // App configuration section
 //app.set('views', 'cloud/views');  // Folder containing view templates
@@ -36,22 +14,50 @@ app.use(express.bodyParser());    // Read the request body into a JS object
 
 var UserItem = Parse.Object.extend("IgUser");
 var WrdEntry = Parse.Object.extend("WrdEntry");
-
-//Circulate through wrd entry
-//Create an item for triad word counts
-//for each combination, 
-//check every other word for it's presence
-// +1 if found
-//store score, move on
+var TriadScore = Parse.Object.extend("TriadScore");
 
 
 // Attach request handlers to routes
 app.get('/user/:id', function(req, res) {
-  // GET http://example.parseapp.com/test?message=hello
+    
+    // var triadScore = new TriadScore();
+    // var processRows = new Parse.Query(WrdEntry);
+    // processRows.find("combos");
+    // processRows.forEach(function(combo){
+    //     for(var i = 0; i < combo.length; i++)
+    //     {
+    //         var triad = combo[i];
+    //         var processCombos = new Parse.Query(WrdEntry);
+    //         processCombos.containedIn("combos", triad);
+    //         processCombos.forEach(function(found)
+    //         {
+    //             triadScore.set("triad", triad);
+    //             triadScore.increment(score);
+    //             triadScore.save();
+    //         });
+    //     }
+    // });
+
+        // processWords.forEach(function(combination){
+        //     var processTriads = new Parse.Query(WrdEntry);
+        //     // processTriads.containedIn("combination",)
+        //     [processTriads whereKey:@"combination" equalTo:triad];
+        //     processTriads.forEach(function(word)
+        //     {
+        //         triadScore.set("triad", triad);
+        //         triadScore.increment("score");
+        //         triadScore.save();
+
+
+        //     });
+        // });
+
+
     ig.searchUser({
     q: req.params.id,
     count: '1'
     }).then(function(httpResponse) {
+
         var userItem;
 
         userid = httpResponse.data["data"][0]["id"];
@@ -67,7 +73,6 @@ app.get('/user/:id', function(req, res) {
                     userItem = new UserItem();
                     userItem.set("userid", userid);
                     userItem.set("igusername", igusername);
-                    // userItem.set("igusername",)
                 }  
                 else 
                 {
@@ -79,6 +84,7 @@ app.get('/user/:id', function(req, res) {
             }
         });
 
+
         var postCount = 10;
         ig.getRecentMediaByUser(userid,{
         count: postCount //Only 20 images allowed per call, use pagination feature
@@ -89,46 +95,18 @@ app.get('/user/:id', function(req, res) {
             var captions = [];
 
             posts.forEach(function(post){
-                if (post["caption"] != null && post["caption"]["text"] != null && post["tags"] != null)
-                {
-                    var cap = post["caption"];
-                    var text = post["caption"]["text"];
-                    text = text.toLowerCase();
-                    var tags = post["tags"];
-                    tags.forEach(function(tag)
-                    {
-                        text = text.replace("#"+tag,"");
-                    });
-
-                    text = text.replace(/[.,!?;:"']/g,"").replace("  "," ");
-                    text = text.split(" ");
-
-                    wrdArray = [];
-                    text.forEach(function(word){
-                        if (word.length > 3 && word[0]!="@")
-                        {
-                            wrdArray.push(word);
-                        }
-                    });
-                }
-                if (wrdArray !== "undefined")
-                {
-                    post["words"] = wrdArray;
-                }
-                else 
-                {
-                    post["words"] =[];
-                }
+                post = cleanPost(post);
             });
 
-
+            var gameWords = [];
+            var gameWordCombos = []; 
+            
             posts.forEach(function(post,index){
                 var processKeys = new Parse.Query(WrdEntry);
                 var keys = [];
-                var gameWords = [];
-                var gameWordCombos = [];
 
-                index++;
+
+                index++; //count # of posts
 
                 processKeys.containedIn("word",post["words"]);
                 processKeys.each(function(obj){
@@ -142,37 +120,74 @@ app.get('/user/:id', function(req, res) {
                         captions.push({image:post["id"],caption:post["words"],wordkeys:keys});
                     }
 
-                    if (index == postCount-1){
+                    if (index == postCount)  //check if last post in loop
+                    {
                         userItem.set("captions",captions);
+                        userItem.set("gameWords", gameWords);
+                        userItem.set("gameWordCombos", gameWordCombos);
                         userItem.save(null, {
+
                             success: function(userItem) {
-                                // res.send(httpResponse.data["data"]);
-                                res.send(gameWords,gameWordCombos);
-                                // res.send(captions);
+                                res.send({words:gameWords,combos:gameWordCombos});
                                 console.log("Success");
                             },
                             error: function(userItem, error) {
                                 console.log('Try another instagram account' + error.message);
                             }
+
                         });
                     }
                 });
             });
         },
         function(error) {
-        console.log(error);
+            res.send("This account is private. Try another.");
         });
     },
     function(error) {
-    console.log(error);
+        res.send(error);
     });
-
 });
 
 app.post('/process_user', function(req, res) {
-  // POST http://example.parseapp.com/test (with request body "message=hello")
-  res.json({ message: req.body.message });  // Send a JSON response
+  res.json({ message: req.body.message });  
 });
 
 // Attach the Express app to your Cloud Code
 app.listen();
+
+
+function cleanPost(post)
+{
+    if (post["caption"] != null && post["caption"]["text"] != null && post["tags"] != null)
+    {
+        var cap = post["caption"];
+        var text = post["caption"]["text"];
+        text = text.toLowerCase();
+        var tags = post["tags"];
+        tags.forEach(function(tag)
+        {
+            text = text.replace("#"+tag,"");
+        });
+
+        text = text.replace(/[.,!?;:"'+()/]/g," ").replace("  "," ");
+        text = text.split(" ");
+
+        wrdArray = [];
+        text.forEach(function(word){
+            if (word.length > 3 && word[0]!="@")
+            {
+                wrdArray.push(word);
+            }
+        });
+    }
+    if (wrdArray !== "undefined")
+    {
+        post["words"] = wrdArray;
+    }
+    else 
+    {
+        post["words"] =[];
+    }
+}
+
