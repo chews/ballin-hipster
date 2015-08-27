a10ddeacb9c583780f535904da11fecb5cce6d39
genevievelesperance <genevieve.lesperance@mail.mcgill.ca>
2015-08-27 09:52:24 -0700

    Cleaned params and wrote command to run

--- cloud/wrdscore/jsonToCSV.py
+++ cloud/wrdscore/jsonToCSV.py
@@ -1,6 +1,7 @@
 # Script to convert 250K WrdEntry dictionary from JSON
 # to CSV with only word, dictionary id, and complexity score
 # Load to Parse database in WrdScore object.
+# $ python jsonToCsv.py WrdEntry.json WrdEntry.csv
 
 import sys
 import json
@@ -9,11 +10,13 @@ json_file = sys.argv[1]
 
 file_data = open(json_file)
 data = json.load(file_data)
+
 out_data = "word,wordEntryId,complexity\n"
-for e in data:
-    complexity = len(e['word']) - 3
-    newe =  e['word'] + "," + e['objectId'] +"," + str(complexity) +"\n"
-    out_data += newe
+
+for entry in data:
+    complexity = len(entry['word']) - 3
+    scoreEntry =  entry['word'] + "," + entry['objectId'] +"," + str(complexity) +"\n"
+    out_data += scoreEntry
 
 with open(sys.argv[2], "w") as out_file:
     out_file.write(out_data)
